8b72b8e406b310695dbf0498ff94e033f278223f
Anthony Tong <atong28.usa@gmail.com>
2025-11-03 16:44:56 -0800

    fix training scripts

--- run_training.sh
+++ run_training.sh
@@ -1,10 +1,12 @@
-
-
-experiment="marina-run-no-warmup-fixed-logs"
+experiment="marina-run-no-warmup-training"
 input_types="{hsqc,c_nmr,h_nmr,mw,mass_spec}"
-for seed in 0 1 2; do
-    experiment_name="${experiment}-num-${seed}"
-    sed -i '' "4s/.*/  name: atong-${experiment_name} /" nautilus/jobs/atong_marina_job.yaml
-    sed -i '' "41s/.*/              pixi run --manifest-path pixi.toml -- torchrun --nproc_per_node=4 stage1.py --input_types ${input_types} --seed 0 --experiment_name ${experiment_name} --no_warmup /" nautilus/jobs/atong_marina_job.yaml
-    kubectl create -f nautilus/jobs/atong_marina_job.yaml
+
+for num in 0 1 2 3 4 5; do
+  experiment_name="${experiment}-${num}"
+
+  sed -i -e "4s~.*~  name: atong-${experiment_name} ~" \
+         -e "41s~.*~              pixi run --manifest-path pixi.toml -- torchrun --nproc_per_node=4 stage1.py --input_types ${input_types} --seed 0 --experiment_name ${experiment_name} --no_warmup ~" \
+         nautilus/jobs/atong_marina_job.yaml
+
+  kubectl create -f nautilus/jobs/atong_marina_job.yaml
 done
